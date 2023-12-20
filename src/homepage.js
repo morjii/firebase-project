7f770b6356122fee6f3ff8f14cba4d42f17e4d1d
Morji <morjimed@gmail.com>
2023-12-20 15:48:54 +0100

    homepage

---
+++ src/homepage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div style={styles.container}>
+            <h1 style={styles.heading}>Firebase Project</h1>
+            <button style={styles.button} onClick={handleLogin}>Se connecter à Google</button>
+        </div>
+    );
+}
+
+
+
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        backgroundColor: '#f7f7f7',
+    },
+    heading: {
+        color: '#333',
+        marginBottom: '20px',
+    },
+    button: {
+        padding: '10px 20px',
+        backgroundColor: '#007bff',
+        color: 'white',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+    }
+};
+
+export default HomePage;
