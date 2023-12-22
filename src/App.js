68beecf896d6f712c21fe74f13fcc5f2c39ff760
Ebou <bienechristy94@gmail.com>
2023-12-22 14:15:27 +0100

    modif

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './homepage';
-import ContactsPage from './ContactsPage';
 
 function App() {
   return (
-    <Router>
-    <Routes>
-      <Route exact path="/" element={<HomePage />} />
-      <Route path="/contacts" element={<ContactsPage />} />
-    </Routes>
-  </Router>
+    <div style={{minHeight:'100vh',display:'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <button style={{padding:'10px 20px', backgroundColor:'#007bff', color:'white', border:'none', borderRadius:'5px', cursor:'pointer' }}>FIREBASE PROJECT</button>
+    </div>
   
   );
 }
